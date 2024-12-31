c045beba225cd081ad005bd8df61014fcc595661
Bintang Adiputra Pratama <bintangadiputra@proton.me>
2024-12-31 16:27:17 +0700

    rust: read version in Nix derivation from Cargo.toml

--- rust/nix/default.nix
+++ rust/nix/default.nix
@@ -3,7 +3,10 @@
 , debug ? false
 }:
 let
-  version = lib.trim (lib.readFile ../VERSION);
+  version = lib.pipe (lib.readFile ../Cargo.toml) [
+    (lib.match "^.*\nversion = \"([0-9]+\.[0-9]+\.[0-9]+)\".*$")
+    lib.head
+  ];
 in
 rustPlatform.buildRustPackage ({
   pname = "foobar";
